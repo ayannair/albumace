e5331160fe5e887603cf75117eff47da5c71f3d1
ayannair <97637942+ayannair@users.noreply.github.com>
2024-08-07 18:02:34 -0400

    implemented mongodb

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
-# Function to get the MongoDB client and database
 def get_db():
-    client = MongoClient('mongodb://localhost:27017/')  # Replace with your MongoDB connection string
-    db = client['fantanosize_db']  # Replace with your database name
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['fantanosize_db']
     return db
