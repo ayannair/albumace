404f945588cf91c1b821d9db1d92a4928dc187cf
ayannair <97637942+ayannair@users.noreply.github.com>
2024-08-08 22:10:09 -0400

    added continuous change in score after saving

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import axios from 'axios';
 import LoadingIcon from './components/LoadingIcon';
-import ScoreBars from './components/ScoreBars';
+import AlbumCard from './components/AlbumCard';
+import CustomAlbumCard from './components/CustomAlbumCard';
 
 const App = () => {
   const [query, setQuery] = useState('');
   const [scores, setScores] = useState(null);
+  const [customScores, setCustomScores] = useState(null);
   const [lyrics, setLyrics] = useState(null);
   const [selectedSong, setSelectedSong] = useState(null);
   const [topics, setTopics] = useState({});
-  const [coverArtUrl, setCoverArtUrl] = useState(null);
-  const [coverArt, setCoverArt] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showTopics, setShowTopics] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  const [editingScores, setEditingScores] = useState(false);
+
+  const handleInputChange = async (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    if (value.length > 1) {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/autocomplete', {
+          params: { query: value }
+        });
+        setSuggestions(response.data);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
 
   const handleSearch = async () => {
     setScores(null);
     setLyrics(null);
     setSelectedSong(null);
     setTopics({});
-    setCoverArtUrl(null);
-    setCoverArt(null);
     setLoading(true);
+    setCustomScores(null);
 
     try {
       const response = await axios.get(`http://127.0.0.1:5000/search?query=${query}`);
       console.log(response.data);
 
-      const { concept_score, features_score, lyrics_score, originality_score, overall_score, production_score, vocals_score } = response.data['score'][2];
+      const { concept_score, features_score, lyrics_score, originality_score, overall_score, production_score, vocals_score } = response.data['score'];
       const lyricsData = response.data['lyrics'];
-      const coverArtPath = response.data['cover_art_url'];
 
       setScores({ concept_score, features_score, lyrics_score, originality_score, overall_score, production_score, vocals_score });
       setLyrics(lyricsData);
-      setCoverArtUrl(coverArtPath);
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
@@ -41,22 +59,8 @@ const App = () => {
     }
   };
 
-  useEffect(() => {
-    if (coverArtUrl) {
-      downloadImage(coverArtUrl).then(imageBlob => {
-        setCoverArt(URL.createObjectURL(imageBlob));
-      });
-    }
-  }, [coverArtUrl]);
-
-  const downloadImage = async (url) => {
-    const response = await fetch(url);
-    const imageBlob = await response.blob();
-    return imageBlob;
-  };
-
   const handleSongClick = async (songTitle) => {
-    setLoading(true);
+    setShowTopics(false);
 
     try {
       const response = await axios.get('http://127.0.0.1:5000/get_topic', {
@@ -68,64 +72,119 @@ const App = () => {
       console.error('Error fetching topic:', error);
     } finally {
       setLoading(false);
+      setTimeout(() => setShowTopics(true), 500);
     }
   };
 
   const sortedLyrics = lyrics ? Object.entries(lyrics).sort(([aIndex], [bIndex]) => aIndex - bIndex) : [];
 
+  const handleSaveScores = async (newScores) => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/save_scores', {
+        title: query,
+        scores: newScores,
+      });
+
+      console.log(response.data);
+      if(response.data['updated_scores']){
+        const { concept_score, features_score, lyrics_score, originality_score, overall_score, production_score, vocals_score } = response.data['updated_scores'];
+        setScores({ concept_score, features_score, lyrics_score, originality_score, overall_score, production_score, vocals_score });
+      }
+    } catch (error) {
+      console.error('Error saving scores:', error);
+    }
+
+    setCustomScores(newScores);
+    setEditingScores(false);
+  };
+
+
+
   return (
     <div className="container">
-      <div className="content">
+      <div className="header">
         <h1>Fantanosize</h1>
         <div className="search-bar">
           <input
             type="text"
             placeholder="Search an Album..."
             value={query}
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={handleInputChange}
           />
           <button onClick={handleSearch}>Search</button>
-        </div>
-        {loading && <LoadingIcon loading={loading} />}
-        {coverArt && <img src={coverArt} alt="Album Cover" className="album-cover" />} {/* Display downloaded cover art */}
-        {scores && <ScoreBars scores={scores} />}
-        
-        {lyrics && (
-          <div className="lyrics-table">
-            <h2>Songs</h2>
-            <table>
-              <tbody>
-                {sortedLyrics.map(([index, { title }]) => (
-                  <tr key={index}>
-                    <td>{title}</td>
-                    <td>
-                      <button
-                        className="get-topics-button"
-                        onClick={() => handleSongClick(title)}
-                      >
-                        {topics[title] ? 'View Topics' : 'Get Topics'}
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
-
-        {selectedSong && topics[selectedSong] && (
-          <div className="topics-section">
-            <h2>Topics for {selectedSong}</h2>
-            <ul>
-              {topics[selectedSong].split('\n').map((topic, index) => (
-                <li key={index}>{topic}</li>
+          {suggestions.length > 0 && (
+            <ul className="autocomplete-suggestions">
+              {suggestions.map((suggestion, index) => (
+                <li
+                  key={index}
+                  onClick={() => {
+                    setQuery(suggestion);
+                    setSuggestions([]);
+                  }}
+                >
+                  {suggestion}
+                </li>
               ))}
             </ul>
-          </div>
+          )}
+        </div>
+      </div>
+      {loading && <LoadingIcon loading={loading} />}
+      <div className="bento-box">
+        <div className="left-section">
+          {lyrics && (
+            <div className="lyrics-table">
+              <h2>Songs</h2>
+              <table>
+                <tbody>
+                  {sortedLyrics.map(([index, { title }]) => (
+                    <tr key={index}>
+                      <td>{title}</td>
+                      <td>
+                        <button
+                          className="get-topics-button"
+                          onClick={() => handleSongClick(title)}
+                        >
+                          {topics[title] ? 'View Topics' : 'Get Topics'}
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+        <div className="right-section">
+          {scores && <AlbumCard scores={scores} />}
+          {selectedSong && topics[selectedSong] && (
+            <div className={`topics-section ${showTopics ? 'fade-in' : 'fade-out'}`}>
+              <h2>{selectedSong} is about...</h2>
+              <ul>
+                {topics[selectedSong].split('\n').map((topic, index) => (
+                  <li key={index}>{topic}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="custom-album-card-container">
+        {scores && !loading && (
+            <button onClick={() => setEditingScores(!editingScores)} className="edit-scores-button">
+              {editingScores ? 'View Scores' : 'Edit Scores'}
+            </button>
+          )}
+        {!loading && (
+          editingScores ? (
+            <CustomAlbumCard initialScores={customScores || scores} onSave={handleSaveScores} />
+          ) : (
+            <AlbumCard scores={customScores || scores} />
+          )
         )}
       </div>
     </div>
-  );
+  );  
 };
 
 export default App;
