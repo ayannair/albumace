e08c55addc65ae8e44b6b590e454d63ef07bf181
ayannair <97637942+ayannair@users.noreply.github.com>
2024-08-21 14:59:48 -0400

    changed mongodb from localhost to atlas

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['fantanosize_db']
+    client = MongoClient("mongodb+srv://<username><password>@score.f3dsl.mongodb.net/?retryWrites=true&w=majority&appName=score")
+    # client = MongoClient('mongodb://localhost:27017/')
+    db = client.get_database('fantanosize_db')
     return db
